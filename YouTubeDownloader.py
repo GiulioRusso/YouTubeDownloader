0f8dcec1c1edecbd6525bdb7c1677c0aa8700a36
Giulio Russo <russogiulio1998@icloud.com>
2024-05-08 20:26:47 +0200

    First commit

---
+++ YouTubeDownloader.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+from sys import argv
+
+if (len(argv) == 1):
+    print("\n >>> WARNING: no link specified \n")
+
+else:
+    # Get the link specified
+    link = argv[1]
+    yt = YouTube(link)
+    print("\n >>> Downloading video: '{}' ".format(yt.title))
+
+    # Download folder
+    download_folder = ""
+
+    # Download video
+    yd = yt.streams.get_highest_resolution()
+    yd.download(download_folder)
+    print("\n >>> Video saved at: '{}' \n".format(download_folder))
---
+++ YouTubeDownloader.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+cd /Applications/YouTubeDownloader
+python3 YouTubeDownloader.py $1
